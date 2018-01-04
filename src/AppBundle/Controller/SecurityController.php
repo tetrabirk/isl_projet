2e0af9ee5839128304a60bb8e9a2198b51af0179
tetrabirk <ludovic.leyens@gmail.com>
2018-01-04 20:39:40 +0100

    login fonctionnel

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -3,16 +3,25 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
     /**
      * @Route("/connexion", name="connexion")
      */
-    public function connexionAction()
+    public function connexion(Request $request, AuthenticationUtils $authUtils)
     {
+        //get login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        //last username enterd by user
+        $lastUsername = $authUtils->getLastUsername();
         return $this->render('security/connexion.html.twig',array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ));
     }
 
