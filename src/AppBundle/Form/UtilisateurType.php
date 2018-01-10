9f09b8e7f796abcf5b074858e5193511be3f92ef
tetrabirk <ludovic.leyens@gmail.com>
2018-01-10 21:18:47 +0100

    form form form

--- src/AppBundle/Form/UtilisateurType.php
+++ src/AppBundle/Form/UtilisateurType.php
@@ -4,6 +4,11 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,21 +20,33 @@ class UtilisateurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('type',ChoiceType::class, array(
+
+            ->add('type', ChoiceType::class, array(
                 'choices' => array(
-                    'Prestataire' => 'Prestataire',
                     'Internaute' => 'Internaute',
+                    'Prestataire' => 'Prestataire',
                 ),
+                'placeholder' => '...',
+                'expanded' => false,
                 'mapped' => false,
             ))
-            ->add('email')
-            ->add('motDePasse')
-            ->add('inscription')
-            ->add('nbEssaisInfructueux')
-            ->add('banni')
-            ->add('confInscription');
+            ->add('email', EmailType::class, array(
+                'required' => true
+            ))
+            ->add('motDePasse', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes doivent être identique',
+                'required' => true,
+                'first_options' => array('label'=>'Mot de Passe'),
+                'second_options' => array('label'=>'Répétez Mot de Passe'),
+            ))
+            ->add('Envoyer', SubmitType::class, array(
+                    'attr' => array('class' => 'btn btn-primary pull-right'),
+                )
+            );
+    }
 
-    }/**
+    /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
